26ecff244bd6083c7c78418eb8bfa127adcdc8e3
Jakns99 <j.schermerhorn99@gmail.com>
2025-04-14 16:07:59 -0700

    Added way to List Top 5 by CPU and Memory.

--- server-stats.sh
+++ server-stats.sh
@@ -13,9 +13,17 @@ free -h | awk '/^Mem/ { printf "Used: %s, Free: %s, Total: %s\n", $3, $4, $2 }'
 echo "Total Disk Usage:"
 df -h | awk '$NF=="/" { printf "Used: %s, Free: %s, Total: %s, Usage: %s\n", $3, $4, $2, $5 }'
 
+# List Top 5 Processes by CPU Usage
+echo "Top 5 Processes by CPU Usage:"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
+
+# List Top 5 Processes by Memory Usage
+echo "Top 5 Processes by Memory Usage:"
+ps -eo pid,comm,%mem --sort=-%mem | head -n 6
+
 # TODO 1. List the Total CPU Usage (DONE)
 # TODO 2. List the Total Memory Usage (Free and Used including percentage) (DONE)
 # TODO 3. List the Total Disk Usage (Free and Used including percentage) (DONE)
-# TODO 4. List Top 5 Processes by CPU Usage
-# TODO 5. List Top 5 Processes by Memory Usage
+# TODO 4. List Top 5 Processes by CPU Usage (DONE)
+# TODO 5. List Top 5 Processes by Memory Usage (DONE)
 # TODO 6. Optional: Add more stats like OS version, uptime, load average, logged in users, failed login attempts, etc.
