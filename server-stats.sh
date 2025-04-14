7df06c40e83a35170d7d393486749dd37b5d865a
Jakns99 <j.schermerhorn99@gmail.com>
2025-04-13 20:17:36 -0700

    Added way to List Total Disk Usage with df.

--- server-stats.sh
+++ server-stats.sh
@@ -9,10 +9,13 @@ mpstat | awk '$3 ~ /[0-9.]+/ { print 100 - $12 }'
 echo "Total Memory Usage:"
 free -h | awk '/^Mem/ { printf "Used: %s, Free: %s, Total: %s\n", $3, $4, $2 }'
 
+# Get Total Disk Usage (Free and Used including percentage)
+echo "Total Disk Usage:"
+df -h | awk '$NF=="/" { printf "Used: %s, Free: %s, Total: %s, Usage: %s\n", $3, $4, $2, $5 }'
 
 # TODO 1. List the Total CPU Usage (DONE)
 # TODO 2. List the Total Memory Usage (Free and Used including percentage) (DONE)
-# TODO 3. List the Total Disk Usage (Free and Used including percentage)
+# TODO 3. List the Total Disk Usage (Free and Used including percentage) (DONE)
 # TODO 4. List Top 5 Processes by CPU Usage
 # TODO 5. List Top 5 Processes by Memory Usage
 # TODO 6. Optional: Add more stats like OS version, uptime, load average, logged in users, failed login attempts, etc.
